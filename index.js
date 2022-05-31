f60cdde5f26f9aefa2caef6c6cc50402bed1fa4e
topiaspe <topiaspe@metropolia.fi>
2022-05-31 11:15:55 +0300

    cypress url back to localhost, pipeline edit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const logger = require('./utils/logger')
 
 const server = http.createServer(app)
 
-const PORT = config.PORT || 3001
+const PORT = config.PORT || 3003
 server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
