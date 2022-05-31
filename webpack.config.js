3656bf33b4ad74f6f5c268e6b88e3601ecdc9ebd
topiaspe <topiaspe@metropolia.fi>
2022-05-31 10:22:16 +0300

    webpack config entry

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebPackPlugin = require('html-webpack-plugin')
 const path = require('path')
 
 module.exports = {
-  entry: "./index.js",
+  entry: "./src/index.jsx",
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
